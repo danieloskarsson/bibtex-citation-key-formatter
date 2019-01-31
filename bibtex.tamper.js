ab105f02a1642ff2e87c8ada29cc718afb8cdd4f
Daniel Oskarsson <daniel.oskarsson@gmail.com>
2019-01-31 21:32:22 +0100

    Improve readability
    And start incrementing version

--- bibtex.tamper.js
+++ bibtex.tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Scholar Bibtex Key
 // @namespace    http://github.com/danieloskarsson/bibtex-citation-key-formatter
-// @version      1
+// @version      6
 // @description  Rename Bibtex citation-keys provided by Google Scholar according to the format "Author1Author2Author3...YYYY"
 // @author       Daniel Oskarsson
 // @match        https://scholar.googleusercontent.com/scholar.bib*
@@ -10,16 +10,18 @@
 
 (function() {
     'use strict';
-    var element = document.getElementsByTagName('pre')[0]
-    var bibtex = element.innerText;
+    let element = document.getElementsByTagName('pre')[0]
+    let bibtex = element.innerText;
+    let authors = /author={(.*)},?\r?\n/.exec(bibtex)[1];
+    let year = /year={(.*)},?\r?\n/.exec(bibtex)[1];
+
     var key = '';
-    /author={(.*)},?\r?\n/.exec(bibtex)[1].split(' and ').forEach(author => {
+    authors.split(' and ').forEach(author => {
         author = author.substring(0, author.indexOf(','))
         author = author.charAt(0).toUpperCase() + author.slice(1)
         author = author.replace(/\s+/g, '');
         key += author;
     });
-    key += /year={(.*)},?\r?\n/.exec(bibtex)[1];
+    key += year;
     element.innerText = bibtex.replace(/@(.*){(.*),/.exec(bibtex)[2], key);
 })();
-
